4d34145dec3249535dec71c5eef8966832dafa4f
mel-th <meleyth@gmail.com>
2025-04-09 11:25:00 -0400

    deploying

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/all-about-me/',  // Updated to match the desired URL
+  base: '/all-about-me/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
